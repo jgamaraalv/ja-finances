937a99b456c588de7b0b200b8170f07596d8eef0
João Amaral <joao.vequiato@iclinic.com.br>
2021-03-18 18:27:10 -0300

    feat: implements Dashboard and Summary components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Summary from "../Summary";
+import { StyledContainer } from "./styles";
+
+const Dashboard = () => {
+  return (
+    <StyledContainer>
+      <Summary />
+    </StyledContainer>
+  );
+};
+
+export default Dashboard;
