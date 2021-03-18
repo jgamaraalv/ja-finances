a82ef06975663b0f82a5355d10104ae4947a5ea4
João Amaral <joao.vequiato@iclinic.com.br>
2021-03-18 18:39:53 -0300

    feat: implements TransactionsTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import Summary from "../Summary";
+import TransactionsTable from "../TransactionsTable";
 import { StyledContainer } from "./styles";
 
 const Dashboard = () => {
   return (
     <StyledContainer>
       <Summary />
+
+      <TransactionsTable />
     </StyledContainer>
   );
 };
