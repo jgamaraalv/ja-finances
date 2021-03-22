fa8ac5a3d078072e76c369009dbef09b1668c2a2
João Amaral <joao.vequiato@iclinic.com.br>
2021-03-22 20:23:31 -0300

    feat: implements NewTransactionModal and form styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal";
 
+import closeImg from "../../assets/close.svg";
+import { StyledContainer } from "./styles";
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,8 +13,31 @@ const NewTransactionModal = ({
   onRequestClose,
 }: NewTransactionModalProps) => {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <StyledContainer>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </StyledContainer>
     </Modal>
   );
 };
