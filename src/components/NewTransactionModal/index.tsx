cb1455b6504169ab32d730cd10757eae88a29b0a
João Amaral <joao.vequiato@iclinic.com.br>
2021-03-22 20:52:03 -0300

    feat: saves form data in state

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -20,6 +20,13 @@ const NewTransactionModal = ({
   onRequestClose,
 }: NewTransactionModalProps) => {
   const [type, setType] = useState("deposit");
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+  function handleCreateNewTransaction(event: React.FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <Modal
@@ -39,11 +46,20 @@ const NewTransactionModal = ({
       <StyledContainer>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Título" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
-        <StyledTransactionTypeContainer>
+        <StyledTransactionTypeContainer onSubmit={handleCreateNewTransaction}>
           <StyledRadioBox
             isActive={type === "deposit"}
             activeColor="green"
@@ -65,7 +81,11 @@ const NewTransactionModal = ({
           </StyledRadioBox>
         </StyledTransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </StyledContainer>
